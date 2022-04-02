2c23a3eec3ad36118257c7623e031dbfd770c05c
KaneWANG0816 <kanewang0816@gmail.com>
2022-04-03 00:23:44 +0800

    init

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-import cv2
-import os
 import argparse
-import glob
-import numpy as np
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
-import matplotlib.pyplot as plt
+import os
 from model import DnCNN
 from utils import *
 
